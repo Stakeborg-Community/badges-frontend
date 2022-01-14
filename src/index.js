48ce8958f772e3e0b74a83c2b072b7d635020ee8
Eugen <eugen.patru@stream-financial.com>
2022-01-14 21:45:17 +0200

    Added chakra into the app

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/react'
+import { Provider } from '@web3-ui/components';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
